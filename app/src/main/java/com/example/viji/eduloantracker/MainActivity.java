49b824b749e694eda192dae1779d5c549bad36c8
Viji Krishnanunni <viji@orangeInfosolutions.com>
2017-03-31 23:18:14 +0530

    first commit

---
+++ app/src/main/java/com/example/viji/eduloantracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.viji.eduloantracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
